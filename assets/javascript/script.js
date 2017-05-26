42569f31a501562c4ef03c16d4d79d7d07ca9c75
Dshollings <dshollings@gmail.com>
2017-05-25 20:07:32 -0700

    indeed api in js file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,17 @@
-Hello everyone!
-Test
+var queryURL = "https://api.indeed.com/ads/apisearch?publisher=4604260559721605&q=" +
+    searchTerm + "&l" + zip + "&format=json&v=2"
+
+  // Performing our AJAX GET request
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+
+
+ // required in html:
+	// in <head>
+		// 	<script type="text/javascript"
+		// src="https://gdc.indeed.com/ads/apiresults.js"></script>
+  	// in <body>                                  
+  		// <span id="indeed_at"><a title="Job Search" href="https://www.indeed.com" rel="nofollow" >jobs by <img alt=Indeed src="https://www.indeed.com/p/jobsearch.gif" style="border: 0; vertical-align: middle;"></a></span>
