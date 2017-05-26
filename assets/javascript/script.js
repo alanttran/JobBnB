a2112692f6f96ab97941cc3be295d3ad2161c8d0
Alan <mr.alan.tran@gmail.com>
2017-05-25 20:41:47 -0700

    updated script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 function initMap() {
-    var myLatLng = { lat: -25.363, lng: 131.044 };
+    var myLatLng = { lat: 32.842674, lng: -117.257767 };
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 11,
         center: myLatLng
     });
 
@@ -13,21 +12,3 @@ function initMap() {
         title: 'Hello World!'
     });
 }
-=======
-
-
-      function initMap() {
-        var myLatLng = {lat: 32.842674, lng:  -117.257767};
-
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 11,
-          center: myLatLng
-        });
-
-        var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: 'Hello World!'
-        });
-      }
->>>>>>> a562dba916dcd3f92850db2476ac56bced92bc41
