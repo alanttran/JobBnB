556fb48ff2d0cfa12eb69a84ed515a7dde1097b6
Alexander Himchak <alexhimchak@Alexanders-MacBook-Pro.local>
2017-06-01 19:40:41 -0700

    pushing script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,37 +1,102 @@
 
 var zip = $("#zip-input");
 var searchTerm = $("#search-term");
+var map;
+var latitude = [];
+var long = [];
+var initialMapP = 0;
 
-var queryURL = "https://api.indeed.com/ads/apisearch?publisher=4604260559721605&q=" +
-    "web+develper" + "&l=" + "92129" + "&format=json&v=2&latlong=1&limit=2"
+$("#user-search-button").click(function(event) {
+  event.preventDefault();
+  var zip = $('#user-zip-code').val();
+  var search = $('#user-job-title').val();
 
-  // Performing our AJAX GET request
-  $.ajax({
-      url: queryURL,
-      method: "GET"
-}).done(function(response){
-  var results = response.data;
-      for (var i = 0; i < results.length; i++) {
-      console.log(results[i].latitude);
-    }
-
+  jobSearch(zip, search);
 });
+
+
+function jobSearch(myLocation, searchTerm) {
+  $.ajax({
+      cache: false,
+      data: $.extend({
+        publisher: '4604260559721605',
+        v: '2',
+        format: 'json',
+        q: searchTerm,
+        l: myLocation,
+        radius: 25,
+        limit: 10,
+        sort: 'date',
+        highlight: 1,
+        filter: 1,
+        latlong: 1,
+      }, {
+        start: 0,
+        end: 10
+      }),
+      dataType: 'jsonp',
+      type: 'GET',
+      timeout: 5000,
+      url: 'http://api.indeed.com/ads/apisearch'
+    })
+    .done(function(searchTerm) {
+      $.each(searchTerm.results, function(i, item) {
+          console.log(item);
+          console.log(item.jobtitle);
+          console.log(item.company);
+          console.log(item.longitude);
+          console.log(item.latitude);
+          console.log(item.url);
+          console.log(item.snippet);
+          console.log(item.date);
+          console.log(item.formattedRelativeTime);
+          console.log(item.expired);
+          var templat = parseFloat(item.latitude)
+          latitude.push(templat);
+          console.log(latitude);
+          var templong = parseFloat(item.longitude)
+          long.push(templong);
+          console.log(long)
+          
+      
+      });
+      initMap();
+    });
+}
+
+
+//jobSearch(92129, "janitor");
+
+  function initMap() {
+
+    if (initialMapP === 0){
+      var myLatLng = {lat: 32.7157, lng: -117.1611};
+      initialMapP++;
+    }
+    else {
   
+    var myLatLng = {lat: latitude[0], lng: long[0]};
+  }
+    var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 9,
+          center: myLatLng
+      });
+    
+     for (var i = 0; i < latitude.length; i++) {
+    	myLatLng = {lat: latitude[i], lng: long[i]};
+     var marker = new google.maps.Marker({
+           position: myLatLng,
+           map: map,
 
-//   function initMap() {
-//   for (var i = 0; i < results.length; i++) {
-//     var myLatLng = {lat:results[i].latitude, lng: results[i].longitude};
-//     var map = new google.maps.Map(document.getElementById('map'), {
-//           zoom: 11,
-//           center: myLatLng
-//       });
+          title: 'Hello World!'
+ 
+   });
+   }
+}
 
-//     var marker = new google.maps.Marker({
-//           position: myLatLng,
-//           map: map,
-//           title: 'Hello World!'
-//   });
-//   }
-// }
+
+
+
+
 
           
