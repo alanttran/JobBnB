6e0da97de07a25469dcd3843ac6e89fb72267fb3
Dshollings <dshollings@gmail.com>
2017-06-03 12:56:22 -0700

    create search and populate modular js file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,32 +4,48 @@ var map;
 var latitude = [];
 var long = [];
 var initialMapP = 0;
+var jobs = [];
 
-//jobSearch(92129, "janitor")
-initMap();
+$("#user-search-button").click(function(event) {
+  event.preventDefault();
+  var zip = $('#user-zip-code').val();
+  var search = $('#user-job-title').val();
 
-function initMap() {
+  jobSearch(zip, search);
+});
 
-    if (initialMapP === 0) {
-        var myLatLng = { lat: 32.7157, lng: -117.1611 };
-        initialMapP++;
-    } else {
+  function initMap() {
 
-        var myLatLng = { lat: latitude[0], lng: long[0] };
+    if (initialMapP === 0){
+      var myLatLng = {lat: 32.7157, lng: -117.1611};
+      initialMapP++;
     }
+    else {
+  
+    var myLatLng = {lat: latitude[0], lng: long[0]};
+  }
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 9,
-        center: myLatLng
-    });
+          zoom: 9,
+          center: myLatLng
+      });
+    
+     for (var i = 0; i < latitude.length; i++) {
+    	myLatLng = {lat: latitude[i], lng: long[i]};
+     var marker = new google.maps.Marker({
+           position: myLatLng,
+           map: map,
 
-    for (var i = 0; i < latitude.length; i++) {
-        myLatLng = { lat: latitude[i], lng: long[i] };
-        var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-
-            title: 'Hello World!'
-
+          title: 'Hello World!'
+  });
+      marker.addListener('click', function() {
+          map.setZoom(10);
+          map.setCenter(marker.getPosition());
+          var infowindow = new google.maps.InfoWindow({
+          content: "Here's a job"
+  });
+      infowindow.open(marker.get('map'), marker);
         });
-    }
+   }
+
 }
+
